a6faeeaee7f7914afbc290ba64d3071b464b0cb2
RenatoBS24 <pro51.ballena@gmail.com>
2026-05-12 00:45:25 -0500

    add Eureka client integration for service registration and discovery

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.26
 
 require (
 	github.com/gofiber/fiber/v2 v2.52.13
+	github.com/xuanbo/eureka-client v0.0.5
 	gorm.io/driver/mysql v1.6.0
 	gorm.io/gorm v1.31.1
 )
@@ -23,6 +24,7 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	github.com/xuanbo/requests v0.0.1 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.20.0 // indirect
 )
--- main.go
+++ main.go
@@ -6,9 +6,18 @@ import (
 	"ubigeo-service/controller"
 
 	"github.com/gofiber/fiber/v2"
+	eureka "github.com/xuanbo/eureka-client"
 )
 
 func main() {
+	client := eureka.NewClient(&eureka.Config{
+		DefaultZone:           "http://localhost:8761/eureka/",
+		App:                   "ubigeo-service",
+		Port:                  8084,
+		RenewalIntervalInSecs: 10,
+		DurationInSecs:        30,
+	})
+	client.Start()
 	db := config.ConnectDatabase()
 	app := fiber.New()
 	api := app.Group("/api/v1/ubigeos")
